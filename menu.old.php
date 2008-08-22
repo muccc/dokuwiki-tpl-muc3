4943bdfa8bc1899d3a990566b59e0695688056c8
codec <codec@e88f35d6-7b98-411f-9960-0f0e3b80c3eb>
2008-08-22 23:03:43 +0000

    git-svn-id: https://brezn.muc.ccc.de/svn/wikitpl/muc3@3 e88f35d6-7b98-411f-9960-0f0e3b80c3eb

---
+++ menu.old.php
@@ -0,0 +1,9 @@
+<ul>
+  <li><?php tpl_link(wl("start"),"start"); ?></li>
+  <li><?php tpl_link(wl("treffen"),"treffen"); ?></li>
+  <li><?php tpl_link(wl("projekte"),"projekte"); ?></li>
+  <li><?php tpl_link(wl("termine"),"termine"); ?></li>
+  <li><?php tpl_link(wl("verein"),"verein"); ?></li>
+  <li><?php tpl_link(wl("wiki:wiki"),"wiki"); ?></li>
+  <li><?php tpl_link(wl("links"),"links"); ?></li>
+</ul>
